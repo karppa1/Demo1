43dd8d1aa46c359b6d116feadd675e2ade5d58e5
karppa1 <K2264@student.jamk.fi>
2016-01-20 19:39:00 +0200

    Olioharjoitus 1 versio 1.0

--- Harjoituksia/Olioharjoituksia/Program.cs
+++ Harjoituksia/Olioharjoituksia/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace Olioharjoituksia
 {
+    class Kiuas
+    {
+        public string Model { get; set; }
+        public int Temp { get; set; }
+        public int Kosteus { get; set; }
+
+        public void AdjustTemp()
+        {
+            Temp += 5;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine(Temp);
+            Console.WriteLine(Kosteus);            
+        }
+        
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
+            Kiuas joku = new Kiuas();
+            joku.Model = "Puu";
+            joku.Temp = 80;
+            joku.Kosteus = 70;
+
+            joku.PrintData();
+
+            joku.AdjustTemp();
+            joku.PrintData();
+
+            Console.ReadLine();
+
         }
+
+
     }
 }
