47f5370627435f3d8d0352ad800d9447af39eb3f
karppa1 <K2264@student.jamk.fi>
2016-01-21 00:12:54 +0200

    KiuasApplication versio 1.0

---
+++ KiuasApplication/Kiuas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiuasApplication
+{
+    class Kiuas
+    {
+        public int Temp { get; set; }
+        public int Moist { get; set; }
+
+        public void SetTemp()
+        {
+            Temp += 15;
+        }
+
+        public void SetMoist()
+        {
+            Moist += 25;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("* Kiukaan tiedot *");
+            Console.WriteLine("Lämpötila: " + Temp + " astetta");
+            Console.WriteLine("Kosteus:" + Moist + " %");
+            Console.WriteLine();
+        }
+
+        ~Kiuas()
+        {
+            Console.WriteLine("Kiuas poistettu!");
+        }
+    }
+}
---
+++ KiuasApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiuasApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas ekakiuas = new Kiuas();
+            ekakiuas.Temp = 60;
+            ekakiuas.Moist = 62;
+            ekakiuas.PrintData();
+
+            Console.WriteLine();
+
+            ekakiuas.SetTemp();
+            ekakiuas.SetMoist();
+            ekakiuas.PrintData();
+
+            Console.ReadLine();
+        }
+    }
+}
