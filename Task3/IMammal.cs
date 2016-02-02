8945e4e479cc35a1af94d8d016f9249056da7298
karppa1 <K2264@student.jamk.fi>
2016-02-02 17:50:56 +0200

    Task3 version 1.0.

---
+++ Task3/IMammal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    abstract class IMammal
+    {
+        public int Age { get; set; }
+        abstract public void Move(int amount);
+
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human mammal = new Human();
+            mammal.Move(15);
+/*
+            for (int i = 0; i < 50; i++)
+            {
+                mammal.GetOlder();
+            }
+*/
+            Console.ReadLine();
+
+        }
+    }
+}
