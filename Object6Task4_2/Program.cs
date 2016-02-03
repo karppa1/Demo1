9816e7d1529ee0b03e5f9cfd04c1b52c00e651fd
karppa1 <K2264@student.jamk.fi>
2016-02-03 17:57:18 +0200

    Object6Task4_2 version 1.02.

--- Object6Task4_2/Program.cs
+++ Object6Task4_2/Program.cs
@@ -17,7 +17,21 @@ namespace Object6Task4_2
                 string ret = "";
                 for (int i = 0; i < length; i++)
                 {
+
+                int r = rand.Next(0, 2);
+                if (r == 0)
+                {
                     ret += (char)rand.Next('A', 'Z');
+                }
+                else if (r == 1)
+                {
+                    ret += (char)rand.Next('a', 'z');
+                }
+                else if (r == 2)
+                {
+                    ret += (char)rand.Next('0', '1');
+                }
+                    
                 }
                 return ret;
             }
@@ -28,7 +42,7 @@ namespace Object6Task4_2
 
                 List<Person> persons = new List<Person>();
 
-                const int personCount = 10000;
+                const int personCount = 100000;
 
                 // start computing elapsed time
                 Stopwatch watch = new Stopwatch();
@@ -57,14 +71,59 @@ namespace Object6Task4_2
                     Person p = persons.Find(x => x.FirstName == nameToFind);
                     if (p != null)
                     {
-                        Console.WriteLine("Found person with firstname " + nameToFind + " : " + p.FirstName + " " + p.LastName);
+                        //Console.WriteLine("Found person with firstname " + nameToFind + " : " + p.FirstName + " " + p.LastName);
                     }
                 }
 
                 watch.Stop();
                 Console.WriteLine("Finding persons took " + watch.ElapsedMilliseconds + "ms");
 
-                Console.ReadLine();
+            Dictionary<string, Person> persons2 = new Dictionary<string, Person>();
+
+            watch.Restart();
+
+            for (int i = 0; i < personCount; i++)
+            {
+                do
+                {
+                    Person p = new Person(RandomString(12),
+                                                RandomString(4),
+                                                RandomString(8),
+                                                rand.Next(1, 100),
+                                                rand.Next(30, 140),
+                                                rand.Next(100, 220));
+                    if (!persons2.ContainsKey(p.FirstName)) 
+                    {
+                        persons2.Add(p.FirstName, p);
+                        break;                        
+                    }
+                    
+
+                } while (true);
+            }
+            watch.Stop();
+            Console.WriteLine("Adding persons to dictionary took " + watch.ElapsedMilliseconds + "ms");
+
+            // find 1000000 names from the dictionary
+
+            watch.Restart();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                string nameToFind = RandomString(4);
+                Person p = null;
+                persons2.TryGetValue(nameToFind, out p);
+                    
+                if (p != null)
+                {
+                    //Console.WriteLine("Found person with firstname " + nameToFind + " : " + p.FirstName + " " + p.LastName);
+                }
+            }
+
+            watch.Stop();
+            Console.WriteLine("Finding persons from dictionary took " + watch.ElapsedMilliseconds + "ms");
+
+            Console.ReadLine();
             }
         }
     }
