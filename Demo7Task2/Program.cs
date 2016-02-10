c6ae429a3c3491db598263019f2cdd9cd9a6cb0e
karppa1 <K2264@student.jamk.fi>
2016-02-10 19:26:56 +0200

    Demo7Task2 version 0.01.

---
+++ Demo7Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo7Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Anna luku > ");
+            string line = Console.ReadLine();
+
+            try
+            {
+                int number = int.Parse(line);
+                Console.WriteLine("Luku oli: " + number);
+
+            } catch(FormatException)
+            {
+                Console.WriteLine("Error!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
