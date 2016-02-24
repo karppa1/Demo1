2144c36f1a8a1e1d44f58134d21c7f1d9d6016b7
karppa1 <K2264@student.jamk.fi>
2016-02-24 19:36:12 +0200

    Demo11 EmployeeApp2 version 0.01.

---
+++ Demo11/EmployeeApp2/Model/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp2.Model
+{
+    /// <summary>
+    /// This class holds employee properties.
+    /// </summary>
+    public class Employee
+    {
+        public string ID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string JobTitle { get; set; }
+        public EmployeeImage Image { get; set; }
+        public string Fullname
+        {
+            get
+            {
+                return Lastname + " " + Firstname;
+            }
+        }
+    }
+
+    /// <summary>
+    /// This class holds employee view model data.
+    /// </summary>
+    public class EmployeeViewModel
+    {
+        private List<Employee> employees = new List<Employee>();
+        public List<Employee> Employees { get { return employees; } }
+
+        public EmployeeViewModel()
+        {
+            // generate dummy data if needed
+        }
+    }
+}
