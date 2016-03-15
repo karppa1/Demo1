6821aa2a84fe05b7a47eb0a97c33558eb48c706e
karppa1 <K2264@student.jamk.fi>
2016-03-15 18:46:09 +0200

    Add Esimerkki and Esimerkki2

---
+++ Demo17/Esimerkki2/Page2.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Esimerkki2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Page2 : Page
+    {
+        public Page2()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter is Person)
+            {
+                Person person = (Person)e.Parameter;
+                textBlock.Text = "Hi, " + person.Firstname + " " + person.Lastname;
+            }
+            else
+            {
+                textBlock.Text = "Hi!";
+            }
+            base.OnNavigatedTo(e);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Random rand = new Random();
+            (App.Current as App).Number = rand.Next();
+
+            // get root frame (which show pages)
+            Frame rootFrame = Window.Current.Content as Frame;
+            // did we get it correctly
+            if (rootFrame == null) return;
+            // navigate back if possible
+            if (rootFrame.CanGoBack)
+            {
+                rootFrame.GoBack();
+            }
+        }
+    }
+}
